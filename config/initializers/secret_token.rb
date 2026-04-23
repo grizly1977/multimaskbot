b4bc9db5b855bcc3a65c927a2c46bca418777b43
grizly1977 <grizly1977@gmail.com>
2026-04-23 16:33:14 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vlad::Application.config.secret_token = 'c7992848adc1d5fdce7cbd268bc1f431e7b6175c74a71a8dc7b8f59c33c7a9352af58ea1d1e7076e213cc494b0ee0c61113cb15c7481388ece6e13ad84484c69'
